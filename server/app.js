7421ed4489d420a52ea0357ce7ca3d5159813c2a
Ananta Ihza <anantaihza21@gmail.com>
2024-08-06 07:40:13 +0700

    feat: install package

---
+++ server/app.js
@@ -0,0 +1,17 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+module.exports = app;
