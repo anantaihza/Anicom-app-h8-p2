f53f871d03d714ba325498d34c052aa25800a261
Ananta Ihza <anantaihza21@gmail.com>
2024-08-06 19:53:59 +0700

    feat: get Subscribe user

--- server/controllers/subscribeController.js
+++ server/controllers/subscribeController.js
@@ -1,10 +1,19 @@
 const axios = require('axios');
-const { Anime, Subscribe, sequelize } = require('../models');
+const { Anime, Subscribe, User, sequelize } = require('../models');
 
 class SubscribeController {
   static async getSubscribe(req, res, next) {
     try {
-      
+      const { id } = req.user;
+      const subscribes = await User.findByPk(id, {
+        attributes: [],
+        include: {
+          model: Anime,
+          as: "anime"
+        }
+      });
+
+      res.status(200).json(subscribes.anime)
     } catch (error) {
       next(error);
     }
@@ -21,45 +30,41 @@ class SubscribeController {
       });
 
       const result = await sequelize.transaction(async (t) => {
-        const [anime, createdAnime] = await Anime.findOrCreate(
-          {
-            where: {
-              mal_id: mal_id,
-            },
-            defaults: {
-              mal_id: mal_id,
-              imageUrl: data.data.images.jpg.image_url,
-              title: data.data.title,
-              episodes: data.data.episodes,
-              status: data.data.status,
-              score: data.data.score,
-              studios: data.data.studios[0].name,
-              synopsis: data.data.synopsis,
-            },
-            transaction: t
-          }
-        );
-
-        const [subscribe, createdSubscribe] = await Subscribe.findOrCreate(
-          {
-            where: {
-              UserId: id,
-              AnimeId: anime.id,
-            },
-            defaults: {
-              UserId: id,
-              AnimeId: anime.id,
-            },
-            transaction: t
+        const [anime, createdAnime] = await Anime.findOrCreate({
+          where: {
+            mal_id: mal_id,
           },
-        );
+          defaults: {
+            mal_id: mal_id,
+            imageUrl: data.data.images.jpg.image_url,
+            title: data.data.title,
+            episodes: data.data.episodes,
+            status: data.data.status,
+            score: data.data.score,
+            studios: data.data.studios[0].name,
+            synopsis: data.data.synopsis,
+          },
+          transaction: t,
+        });
+
+        const [subscribe, createdSubscribe] = await Subscribe.findOrCreate({
+          where: {
+            UserId: id,
+            AnimeId: anime.id,
+          },
+          defaults: {
+            UserId: id,
+            AnimeId: anime.id,
+          },
+          transaction: t,
+        });
 
         return subscribe;
       });
 
       console.log(result);
       res.status(201).json({
-        message: 'Success to subscribe anime',
+        message: `Success to subscribe ${data.data.title}`,
         result,
       });
     } catch (error) {
--- server/models/user.js
+++ server/models/user.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      User.belongsToMany(models.Anime, { through: models.Subscribe });
+      User.belongsToMany(models.Anime, { through: models.Subscribe, as: "anime" });
       User.hasMany(models.Subscribe);
     }
   }
