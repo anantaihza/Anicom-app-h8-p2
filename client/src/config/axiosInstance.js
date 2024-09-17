5fbcf01f68b771b19bf5f61527dc3dcee89b5b93
Ananta Ihza <anantaihza21@gmail.com>
2024-09-17 17:03:00 +0700

    feat: switch baseURL to local

--- client/src/config/axiosInstance.js
+++ client/src/config/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://i-project.anantair.my.id',
-  // baseURL: 'http://localhost:3000',
+  // baseURL: 'https://i-project.anantair.my.id',
+  baseURL: 'http://localhost:3000',
 });
 
 export default instance;
